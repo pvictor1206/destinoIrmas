e569f8124fa63f006d37e49c11018b3e86ec1b45
Wesley Barbosa <72519926+pvictor1206@users.noreply.github.com>
2023-07-12 14:13:23 -0300

    commit

--- character/player.gd
+++ character/player.gd
@@ -17,13 +17,17 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-		animation.play("run")
-		#altera a direção
-		animation.scale.x = direction * 5.156
+	
+	if Global.counterScene == 8:
+		animation.play("morte_state")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animation.play("idle")
+		if direction:
+			velocity.x = direction * SPEED
+			animation.play("run")
+			#altera a direção
+			animation.scale.x = direction * 5.156
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			animation.play("idle")
 
 	move_and_slide()
